22dfe88c3adbe913890b9b91af529b704653b2b4
ElishaBartov <elisha.bartov@emerson.com>
2022-03-20 00:09:50 +0200

    Finish Color Picker Project git add .!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Class withPrivate variables
+// Class with Private variables
 class RgbColor {
     #redColor = 255;
     #greenColor = 255;
@@ -46,9 +46,9 @@ class RgbColor {
     }
 
     generateHexColor() {
-        let redHexValue = 0;
-        let greenHexValue = 0;
-        let blueHexValue = 0;
+        let redHexValue = '';
+        let greenHexValue = '';
+        let blueHexValue = '';
 
         if(this.#redColor < 16) {
             redHexValue = '0' + (this.#redColor).toString(16);
@@ -78,92 +78,97 @@ class RgbColor {
     }
 }
 
-// Class with regular variables
-// class RgbColor {
-//     constructor(redColor,greenColor,blueColor,hexValue,colorName) {
-//         this.redColor = redColor;
-//         this.greenColor = greenColor;
-//         this.blueColor = blueColor;
-//         this.hexValue = hexValue;
-//         this.colorName = colorName;
-//     }
-
-//     generateHexColor() {
-//         let redHexValue = 0;
-//         let greenHexValue = 0;
-//         let blueHexValue = 0;
-
-//         if(this.redColor < 16) {
-//             redHexValue = '0' + (this.redColor).toString(16);
-//         }
-//         else {
-//             redHexValue = ((this.redColor).toString(16));
-//         }
-//         redHexValue = redHexValue.toUpperCase();
-
-//         if(this.greenColor < 16) {
-//             greenHexValue = '0' + (this.greenColor).toString(16);
-//         }
-//         else {
-//             greenHexValue = (this.greenColor).toString(16);
-//         }
-//         greenHexValue = greenHexValue.toUpperCase();
-
-//         if(this.blueColor < 16) {
-//             blueHexValue = '0' + (this.blueColor).toString(16);
-//         }
-//         else {
-//             blueHexValue = (this.blueColor).toString(16);
-//         }
-//         blueHexValue = blueHexValue.toUpperCase();
-
-//         return '#' + redHexValue + greenHexValue + blueHexValue;
-//     }
-// }
-
-newRgbColor = new RgbColor();
+let previousRgbColor = new RgbColor();
+let colorsHistoryMap = new Map();
 
 function generateRgbColor() {
+    let newRgbColor = new RgbColor(); 
+
     displayPreviousRgbColor();
     
     // Get new RGB Color from input
     newRgbColor.redColor = +document.getElementById('validationCustom01').value;
     newRgbColor.greenColor = +document.getElementById('validationCustom02').value;
     newRgbColor.blueColor = +document.getElementById('validationCustom03').value;
-    newRgbColor.hexValue = newRgbColor.generateHexColor();
     newRgbColor.colorName = document.getElementById('validationCustom04').value;
+    newRgbColor.hexValue = newRgbColor.generateHexColor();    
 
-    displayNewRgbColor();
+    addColorToHistoryMap(newRgbColor);
+
+    // Update previousRgbColor
+    // Get Data from colors history Map
+    previousRgbColor.redColor = colorsHistoryMap.get(newRgbColor.colorName).redColor;
+    previousRgbColor.greenColor = colorsHistoryMap.get(newRgbColor.colorName).greenColor;
+    previousRgbColor.blueColor = colorsHistoryMap.get(newRgbColor.colorName).blueColor;
+    previousRgbColor.colorName = colorsHistoryMap.get(newRgbColor.colorName).colorName;
+    previousRgbColor.hexValue = colorsHistoryMap.get(newRgbColor.colorName).hexValue;
+
+    fillColorsDropdownList();
+
+    displayNewRgbColor(newRgbColor);
 }
 
 function displayPreviousRgbColor() {
     const previousColorViwerElement = document.getElementById('previous-color-viewer');
-    document.getElementsByTagName('h5')[1].remove();
+    document.getElementById('previous-header-dark-mode').remove();
     document.getElementById('previous-color-box').remove();
 
     // Add new color viwer elements
-    if(newRgbColor.colorName == undefined) {
-        newRgbColor.colorName = '';
+    if(previousRgbColor.colorName == undefined) {
+        previousRgbColor.colorName = '';
     }
 
-    if(newRgbColor.hexValue == undefined) {
-        newRgbColor.hexValue = '#'
+    if(previousRgbColor.hexValue == undefined) {
+        previousRgbColor.hexValue = ''
     }
 
-    previousColorViwerElement.innerHTML += `<h5 id="new-header-dark-mode">Previous RGB Color (Color Name: ${newRgbColor.colorName}, Hex: ${newRgbColor.hexValue})</h5>`;
+    previousColorViwerElement.innerHTML += `<h5 id="previous-header-dark-mode">Previous RGB Color (Color Name: ${previousRgbColor.colorName}, RGB(${previousRgbColor.redColor},${previousRgbColor.greenColor},${previousRgbColor.blueColor}), Hex Value: ${previousRgbColor.hexValue})</h5>`;
 
-    previousColorViwerElement.innerHTML += `<div id="previous-color-box" class="color-box" style="background-color: rgb(${newRgbColor.redColor}, ${newRgbColor.greenColor}, ${newRgbColor.blueColor});"></div>`
+    previousColorViwerElement.innerHTML += `<div id="previous-color-box" class="color-box" style="background-color: rgb(${previousRgbColor.redColor}, ${previousRgbColor.greenColor}, ${previousRgbColor.blueColor});"></div>`
 }
 
-function displayNewRgbColor() {
+function displayNewRgbColor(newRgbColor) {
     const newColorViwerElement = document.getElementById('new-color-viewer');
-    document.getElementsByTagName('h5')[0].remove();
+    document.getElementById('new-header-dark-mode').remove();
     document.getElementById('new-color-box').remove();
 
     // Add new color viwer elements
-    newColorViwerElement.innerHTML += `<h5 id="new-header-dark-mode">New RGB Color (Color Name: ${newRgbColor.colorName}, Hex: ${newRgbColor.hexValue})</h5>`;
+    newColorViwerElement.innerHTML += `<h5 id="new-header-dark-mode">New RGB Color (Color Name: ${newRgbColor.colorName}, RGB(${newRgbColor.redColor},${newRgbColor.greenColor},${newRgbColor.blueColor}), Hex Value: ${newRgbColor.hexValue})</h5>`;
 
     newColorViwerElement.innerHTML += `<div id="new-color-box" class="color-box" style="background-color: rgb(${newRgbColor.redColor}, ${newRgbColor.greenColor}, ${newRgbColor.blueColor});"></div>`
+}
+
+function addColorToHistoryMap(newRgbColor) {
+    colorsHistoryMap.set(newRgbColor.colorName,newRgbColor)
+}
+
+// This function gets colors from the colors history Map and sets it to inner HTML dropdown list
+function fillColorsDropdownList() {
+    const dropdownColorsElement = document.getElementById('dropdown-list');
+
+    // Clear old colors history list
+    while(document.getElementById('li-color-row')) {
+        let listElement = document.getElementById('li-color-row');
+        listElement.remove();
+    }
+
+    // Print new colors history list
+    for(let color of colorsHistoryMap.keys()) {
+        dropdownColorsElement.innerHTML += `<li id="li-color-row"><a class="dropdown-item" href="#" onclick="showColorFromHistory('${color}')">${color}</a></li>`;        
+    }
+}
+
+function showColorFromHistory(color) {
+    let newRgbColor = new RgbColor();
+
+    // Get Data from colors history Map
+    newRgbColor.redColor = colorsHistoryMap.get(color).redColor;
+    newRgbColor.greenColor = colorsHistoryMap.get(color).greenColor;
+    newRgbColor.blueColor = colorsHistoryMap.get(color).blueColor;
+    newRgbColor.colorName = colorsHistoryMap.get(color).colorName;
+    newRgbColor.hexValue = colorsHistoryMap.get(color).hexValue;
+
+    displayNewRgbColor(newRgbColor);
 }
 
 // Change Dark/Light mode function
